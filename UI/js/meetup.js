ee6c0321bb235b857ec087a5fc3d1dc193b3f1dd
Micah Akpan <mickey4sure2k7@gmail.com>
2019-01-07 21:20:44 +0100

    [Feature #162646229] Remove modal UI for questions

--- UI/js/meetup.js
+++ UI/js/meetup.js
@@ -6,6 +6,9 @@
   const mPhotosWrapper = d.querySelector('.meetup-photos__wrapper');
   const uploadBtn = d.querySelector('label[role="button"]');
   const fileInput = d.querySelector('input[type="file"]');
+  const askQuestionWrapper = d.querySelector('#ask-question');
+  const postQuestionDirArea = d.querySelector('#post-questions-directive');
+  const askGroupBtn = d.querySelector('.ask-group-btn');
 
   fileInput.onchange = (e) => {
     const image = new Image();
@@ -40,5 +43,30 @@
         addTagsContainer.appendChild(span);
       }
     })
+  }
+
+  askGroupBtn.onclick = function() {
+    showEl({
+      elem: askQuestionWrapper,
+      activeClassName: 'active'
+    });
+    // User should not see
+    // post question directive
+    // after enabling ask questions UI
+    postQuestionDirArea.classList.add('inactive');
+    const divider = d.createElement('hr');
+    divider.classList.add('divider');
+    askQuestionWrapper.appendChild(divider);
+  }
+
+  /**
+   * 
+   * @param {*} elem an object with Element and activeClassName props
+   * @returns {Element} elem
+   * @description Shows an hidden element, applying the styles in activeClassName to it
+   */
+  const showEl = ({ elem, activeClassName }) => {
+    elem.classList.add(activeClassName);
+    return elem;
   }
 })();
