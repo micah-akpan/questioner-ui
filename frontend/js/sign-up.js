129833d92308dfce02e55060d98f5f07cf5ab4e3
Micah Akpan <mickey4sure2k7@gmail.com>
2019-01-23 13:16:13 +0100

    [Feature #163407602] Auth user when user signs up

--- frontend/js/sign-up.js
+++ frontend/js/sign-up.js
@@ -1,4 +1,51 @@
 window.onload = () => {
   /* eslint-disable */
   const userToken = localStorage.getItem('userToken');
+  if (userToken) {
+    window.location.href = './meetups.html';
+  } else {
+    const
+      signUpForm = document.getElementById('sign-up-form'),
+      emailField = document.querySelector('input[name=email]'),
+      passwordField = document.querySelector('input[name=password]'),
+      lastNameField = document.querySelector('input[name=lastname]'),
+      firstNameField = document.querySelector('input[name=firstname]'),
+      userFeedback = document.querySelector('.user-feedback');
+
+    signUpForm.onsubmit = (e) => {
+
+      e.preventDefault();
+
+      const userEmail = emailField.value;
+      const userPassword = passwordField.value;
+      const lastName = lastNameField.value;
+      const firstName = firstNameField.value;
+
+      fetch('http://localhost:9999/api/v1/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        mode: 'cors',
+        body: JSON.stringify({
+          email: userEmail,
+          password: userPassword,
+          firstname: firstName,
+          lastname: lastName
+        })
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          if (res.status === 201) {
+            window.location.href = './meetups.html';
+          } else {
+            userFeedback.textContent = res.error;
+          }
+        })
+        .catch((err) => {
+          // handle error
+        })
+    }
+
+  }
 };
