238aa275830f753a7ae77eae17e27f53599db8e5
Micah Akpan <mickey4sure2k7@gmail.com>
2019-01-23 01:52:56 +0100

    [Feature #163407504] Set prettier to format code with single quotes

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,28 +1,26 @@
-const loginForm = document.getElementById("login-form");
-const userFeedback = document.querySelector(".user-input-feedback");
-const emailField = document.getElementById("userEmail");
-const passwordField = document.getElementById("userPwd");
+const loginForm = document.getElementById('login-form');
+const userFeedback = document.querySelector('.user-input-feedback');
+const emailField = document.getElementById('userEmail');
+const passwordField = document.getElementById('userPwd');
 
 loginForm.onsubmit = function submitForm(e) {
   e.preventDefault();
-  fetch("http://localhost:9999/api/v1/auth/login", {
-    method: "POST",
-    mode: "cors",
-    cache: "no-cache",
+  fetch('http://localhost:9999/api/v1/auth/login', {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
     headers: {
-      "Content-Type": "application/json"
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify({
       email: emailField.value,
       password: passwordField.value
     })
   })
-    .then(response => {
-      return response.json();
-    })
+    .then(response => response.json())
     .then(response => {
       if (response.status === 201) {
-        window.location.href = "./meetups.html";
+        window.location.href = './meetups.html';
       } else {
         userFeedback.textContent = response.error;
       }
