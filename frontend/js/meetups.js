7cf3b35fcc895fe7f0628c621524226014fbc135
Micah Akpan <mickey4sure2k7@gmail.com>
2019-01-30 12:58:06 +0100

    [Feature #163407699] Refactor code

--- frontend/js/meetups.js
+++ frontend/js/meetups.js
@@ -1,27 +1,13 @@
-/* eslint-disable */
-
 const d = document;
 
 const searchIcon = d.getElementById('search-icon');
 const searchBar = d.getElementById('search-bar');
-
 const btnTrigger = d.querySelector('.dropdown-trigger-btn');
 const dropDownMenu = d.querySelector('.q-user-profile__dropdown-menu');
-
-const meetupDropdownTrigger = d.querySelector('.q-card__primary-options');
-const meetupDropdownMenu = d.querySelector(
-  '.q-card__primary-options .dropdown-menu'
-);
-const delBtn = d.querySelector('.dropdown-menu .delete-option');
-const editBtn = d.querySelector('.dropdown-menu .edit-option');
-const modal = d.querySelector('.modal');
-const closeModalBtn = d.querySelector('.close-modal-btn');
-
-
-// Meetups Cards
 const cards = d.querySelector('.cards');
-const cardsWrapper = d.querySelector('.meetup-cards__wrapper');
-const seeMoreBtn  = d.querySelector('.see-more-meetups_btn');
+const seeMoreBtn = d.querySelector('.see-more-meetups_btn');
+
+const apiBaseURL = 'http://localhost:9999/api/v1';
 
 // Toggle display of dropdown menu
 btnTrigger.onclick = () => {
@@ -32,78 +18,35 @@ searchIcon.onclick = () => {
   searchBar.classList.add('show');
 };
 
-// close search bar
-d.addEventListener('keydown', (e) => {
-  if (e.key === 'Escape') {
-    hideNode(searchBar, 'show');
-    hideNode(modal, 'active');
-  }
-});
+/**
+ * @function createMeetupLink
+ * @returns {HTMLElement} Returns an anchor link
+ */
+const createMeetupLink = () => {
+  const meetupCardLink = document.createElement('a');
+  meetupCardLink.setAttribute('href', './meetup.html');
+  return meetupCardLink;
+};
 
-function hideNode(node, className) {
-  node.classList.remove(className);
-}
-
-function showNode(node, keyPressed) {
-  if (keyPressed === 'Escape') {
-    node.classList.add('show');
-  }
-}
-
-window.addEventListener('load', () => {
-  const userToken = localStorage.getItem('userToken');
-  if (!userToken) {
-    window.location.href = './sign-in.html';
-  } else {
-    showAllMeetups(userToken);
-  }
-});
-
-function showAllMeetups(userToken) {
-  fetch('http://localhost:9999/api/v1/meetups', {
-    headers: {
-      Authorization: `Bearer ${userToken}`
-    }
-  })
-    .then((res) => res.json())
-    .then((res) => {
-      if (res.status === 200) {
-        const meetups = res.data;
-        const MAX_MEETUPS = 6;
-        if (meetups.length > MAX_MEETUPS) {
-          const data = meetups.slice(0, MAX_MEETUPS);
-          addMeetupsToDOM(data);
-          seeMoreBtn.textContent = `SEE MORE ${meetups.length - MAX_MEETUPS} MEETUPS`
-        } else {
-          addMeetupsToDOM(meetups);
-        }
-        
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-}
-
-function getTokenHeader(token) {
-  return {
-    Authorization: `Bearer ${token}`
-  }
-}
-
-
+/**
+ * @func getTotalQuestionsAsked
+ * @param {*} meetup Meetup
+ * @returns {Promise<Number>} Returns the total number of questions
+ * asked in a meetup
+ */
 async function getTotalQuestionsAsked(meetup) {
   try {
     let totalQuestions = 0;
     const response = await fetch(
-      `http://localhost:9999/api/v1/meetups/${meetup.id}/questions`, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('userToken')}`
+      `${apiBaseURL}/meetups/${meetup.id}/questions`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('userToken')}`
+        }
       }
-    });
+    );
     const res = await response.json();
     if (res.status === 200) {
-      totalQuestions = res.data.length; 
+      totalQuestions = res.data.length;
     }
 
     return totalQuestions;
@@ -113,26 +56,59 @@ async function getTotalQuestionsAsked(meetup) {
 }
 
 /**
- * @func createDOMElement
- * @param {String} elType The type of the element
- * @param {*} attrs An hash structure with attrs: `className`, and `id`
- * @returns {HTMLElement}
+ * @const numMonthToStr
+ * @description A hash of month ordinal numbers to their short forms
  */
-function createDOMElement(elType, attrs) {
-  const el = document.createElement(elType);
-  const { className, id } = attrs;
-  el.classList.add(className);
-  el.id = id;
-  return el;
+const numMonthToStr = {
+  1: 'Jan',
+  2: 'Feb',
+  3: 'Mar',
+  4: 'Apr',
+  5: 'May',
+  6: 'Jun',
+  7: 'Jul',
+  8: 'Aug',
+  9: 'Sept',
+  10: 'Oct',
+  11: 'Nov',
+  12: 'Dec'
+};
+
+/**
+ * @function getMonth
+ * @param {Number} date
+ * @returns {String} A string version of date e.g 1 -> Jan
+ */
+function getMonth(date) {
+  return numMonthToStr[date];
 }
 
-function createMeetupPrimarySec(meetup) {
+/**
+ * @function parseDate
+ * @param {String} date
+ * @returns {Array} Returns an array of 2 elements (short form of the month and day)
+ */
+const parseDate = (date) => {
+  const currentDate = new Date(date);
+  const month = currentDate.getMonth();
+  const monthShortForm = getMonth(month + 1);
+  const day = currentDate.getDay();
+
+  return [monthShortForm, day];
+};
+
+/**
+ * @function createMeetupPrimarySec
+ * @param {*} meetup Meetup object
+ * @returns {HTMLElement} A wrapper HTMLElement for primary details about a meetup
+ */
+const createMeetupPrimarySec = (meetup) => {
   const content = document.createElement('div');
   content.classList.add('content');
   const meetupImage = document.createElement('img');
   const defaultImage = '../assets/img/startup-meetup2.jpg';
   meetupImage.setAttribute('src', meetup.images[0] || defaultImage);
-  meetupImage.setAttribute('alt', '')
+  meetupImage.setAttribute('alt', '');
   meetupImage.setAttribute('class', 'meetup-main-image');
 
   const meetupQuestionCount = document.createElement('span');
@@ -142,15 +118,15 @@ function createMeetupPrimarySec(meetup) {
   getTotalQuestionsAsked(meetup)
     .then((value) => {
       meetupQuestionCount.textContent = value;
-    })
+    });
 
   content.appendChild(meetupImage);
   content.appendChild(meetupQuestionCount);
 
   return content;
-}
+};
 
-function createMeetupSecondarySec(meetup) {
+const createMeetupSecondarySec = (meetup) => {
   const content = document.createElement('div');
   content.setAttribute('class', 'q-card__sec');
 
@@ -160,25 +136,21 @@ function createMeetupSecondarySec(meetup) {
 
   const meetupDate = document.createElement('p');
   meetupDate.setAttribute('class', 'meetup-sched-date');
-  const [month, day] = parseDate(meetup.happeningOn); 
+  const [month, day] = parseDate(meetup.happeningOn);
   meetupDate.textContent = `${month} ${day}`;
 
   content.appendChild(meetupTitle);
   content.appendChild(meetupDate);
 
   return content;
-}
+};
 
-function createMeetupLink(meetup) {
-  const meetupCardLink = document.createElement('a');
-  // TODO: Use dynamic link here
-  meetupCardLink.setAttribute('href', './meetup.html');
-  return meetupCardLink;
-}
-
-// creates the meetup card
-// and its content
-function createMeetup(meetup) {
+/**
+ * @function createMeetupCard
+ * @param {*} meetup Meetup object
+ * @returns {HTMLElement} Meetup Card
+ */
+const createMeetupCard = (meetup) => {
   const meetupCard = document.createElement('div');
   meetupCard.classList.add('q-card');
 
@@ -197,58 +169,56 @@ function createMeetup(meetup) {
   meetupCard.appendChild(meetupCardLink);
 
   return meetupCard;
-}
+};
 
-function convertMeetupsToCards(meetups) {
-  const allMeetups = meetups.map(meetup => {
-    return createMeetup(meetup);
-  });
-
-  return allMeetups;
-}
+const convertMeetupsToCards = meetups => meetups.map(meetup => createMeetupCard(meetup));
 
 /**
- * 
- * @param {Array<HTMLElement>} meetups 
+ * @func addMeetupsToDOM
+ * @param {Array<HTMLElement>} meetups An array of meetups cards
+ * @returns {HTMLElement} The list of meetup cards
  */
-function addMeetupsToDOM(meetups) {
+const addMeetupsToDOM = (meetups) => {
   cards.innerHTML = '';
   const meetupsDOM = convertMeetupsToCards(meetups);
   meetupsDOM.forEach((meetup) => {
     cards.appendChild(meetup);
   });
-}
 
-
-function createButton({ attribs, text }) {
-  const button = document.createElement('button');
-  button.classList.add('q-btn');
-  button.classList.add('btn__centered');
-  button.textContent = 'SEE MORE X Meetups';
-
-  return button;
-}
-
-function parseDate(date) {
-  const currentDate = new Date(date);
-  const month = currentDate.getMonth();
-  const monthShortForm = getMonth(month + 1);
-  const day = currentDate.getDay();
-
-  return [monthShortForm, day];
-}
-
-const numMonthToStr = { 
-  1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr',
-  5: 'May', 6: 'Jun', 7: 'Jul', 8: 'Aug',
-  9: 'Sept', 10: 'Oct', 11: 'Nov', 12: 'Dec'
+  return cards;
 };
 
-/**
- * @func getMonth
- * @param {Number} date 
- * @returns {String} A string version of date e.g 1 -> Jan
- */
-function getMonth(date) {
-  return numMonthToStr[date];
-}
+const showAllMeetups = (userToken) => {
+  fetch(`${apiBaseURL}/meetups`, {
+    headers: {
+      Authorization: `Bearer ${userToken}`
+    }
+  })
+    .then(res => res.json())
+    .then((res) => {
+      if (res.status === 200) {
+        const meetups = res.data;
+        const MAX_MEETUPS = 6;
+        if (meetups.length > MAX_MEETUPS) {
+          const data = meetups.slice(0, MAX_MEETUPS);
+          addMeetupsToDOM(data);
+          const remainingMeetups = meetups.length - MAX_MEETUPS;
+          seeMoreBtn.textContent = `SEE MORE ${remainingMeetups} ${remainingMeetups > 1 ? 'MEETUPS' : 'MEETUP'}`;
+        } else {
+          addMeetupsToDOM(meetups);
+        }
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+window.addEventListener('load', () => {
+  const userToken = localStorage.getItem('userToken');
+  if (!userToken) {
+    window.location.assign('./sign-in.html');
+  } else {
+    showAllMeetups(userToken);
+  }
+});
