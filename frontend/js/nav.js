8f1c9238af893cd64506764a7d4fd469251b896f
Micah Akpan <mickey4sure2k7@gmail.com>
2019-02-11 07:00:49 +0100

    [Feature #163408470] Refactor nav codebase

---
+++ frontend/js/nav.js
@@ -0,0 +1,162 @@
+const qNavMenuWrapper = document.querySelector('.sidebar-menu__wrapper');
+const logOutButtons = document.querySelectorAll('.logout__btn');
+const dropDownTriggerButton = document.querySelector('.dropdown-trigger-btn');
+const dropDownMenu = document.querySelector('.q-user-profile__dropdown-menu');
+const qNavMenuIconWrapper = document.getElementById('mobile-nav-sidebar__wrapper');
+
+const baseURL = 'http://localhost:9999/api/v1';
+const nav = document.querySelector('.q-right-nav');
+
+const rightNavSpec = [
+  {
+    id: 1,
+    type: 'notifications',
+    src: '../assets/icons/notifications-button.svg',
+    classNames: ['q-btn'],
+    idText: ''
+  },
+
+  {
+    id: 2,
+    type: 'profile',
+    src: '../assets/icons/avatar1.svg',
+    classNames: ['q-btn', 'dropdown-trigger-btn'],
+    idText: 'dropdown-trigger-btn'
+  }
+];
+
+
+document.onkeydown = (e) => {
+  if (e.key === 'Escape') {
+    qNavMenuWrapper.classList.remove('nav-menu-show');
+    qNavMenuIconWrapper.classList.toggle('change');
+    dropDownMenu.classList.remove('show');
+  }
+};
+
+/**
+ * @returns {HTMLElement} Returns the dropdown HTML element
+ */
+const toggleDropDownMenu = () => {
+  dropDownMenu.classList.toggle('show');
+  return dropDownMenu;
+};
+
+if (dropDownTriggerButton) {
+  dropDownTriggerButton.onclick = toggleDropDownMenu;
+}
+
+// Enable all logout buttons to logout a user
+logOutButtons.forEach((logOutBtn) => {
+  logOutBtn.onclick = (e) => {
+    e.preventDefault();
+    logOutUser();
+  };
+});
+
+/**
+ * @param {Number} userId
+ * @returns {Promise<User>} Returns the user with id: `userId`
+ */
+const getUserData = async (userId) => {
+  try {
+    const response = await fetch(`${baseURL}/users/${userId}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('userToken')}`
+      }
+    });
+    const responseBody = await response.json();
+    const { status, data } = responseBody;
+    return status === 200 ? data[0] : null;
+  } catch (e) {
+    throw e;
+  }
+};
+
+/**
+ * @func createRightNavList
+ * @param {Array} navSpec
+ * @returns {HTMLUListElement} Returns a new nav list
+ */
+const createRightNavList = (navSpec) => {
+  const navList = document.createElement('ul');
+  const navListItems = navSpec.map((spec) => {
+    const li = document.createElement('li');
+    const button = document.createElement('button');
+    button.classList.add(...spec.classNames);
+    button.title = spec.type;
+    const img = document.createElement('img');
+
+
+    if (spec.type === 'profile') {
+      li.classList.add('q-user-profile__dropdown-trigger');
+    }
+
+    getUserData()
+      .then((user) => {
+        if (user) {
+          const { avatar, firstname } = user;
+          if (avatar) {
+            img.src = avatar;
+            img.alt = spec.type === 'profile' ? firstname : 'A light yellow colored bell';
+          } else {
+            img.src = spec.src;
+            img.alt = spec.type === 'profile' ? firstname : 'A light yellow colored bell';
+          }
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+
+    if (spec.type === 'profile') {
+      button.onclick = toggleDropDownMenu;
+    }
+
+    button.appendChild(img);
+    li.appendChild(button);
+
+    return li;
+  });
+
+  navListItems.forEach((item) => {
+    navList.appendChild(item);
+  });
+  return navList;
+};
+
+qNavMenuIconWrapper.onclick = function toggleMobileNav() {
+  this.classList.toggle('change');
+  qNavMenuWrapper.classList.toggle('nav-menu-show');
+};
+
+if (window.location.pathname !== '/') {
+  // This dynamic creates the content of the right navigation
+  const urlPath = window.location.pathname.split('/');
+  if (urlPath.includes('admin')) {
+    const navSpec = [
+      {
+        id: 1,
+        type: 'notifications',
+        src: '../../assets/icons/notifications-button.svg',
+        classNames: ['q-btn'],
+        idText: ''
+      },
+
+      {
+        id: 2,
+        type: 'profile',
+        src: '../../assets/icons/avatar1.svg',
+        classNames: ['q-btn', 'dropdown-trigger-btn'],
+        idText: 'dropdown-trigger-btn'
+      }
+    ];
+
+    const rightNav = createRightNavList(navSpec);
+    nav.appendChild(rightNav);
+  } else {
+    const rightNav = createRightNavList(rightNavSpec);
+    nav.appendChild(rightNav);
+  }
+}
