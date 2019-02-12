b557764bd33dd5732acb8fe1de70224342e1f12c
Micah Akpan <mickey4sure2k7@gmail.com>
2019-02-12 20:05:34 +0100

    [Feature #163783432] Implement meetup images upload feature

--- frontend/js/admin/meetup-admin.js
+++ frontend/js/admin/meetup-admin.js
@@ -1,83 +1,87 @@
-/* eslint-disable */
 
-(() => {
-  const d = document;
 
-  const mPhotosWrapper = d.querySelector('.meetup-photos__wrapper');
-  const uploadBtn = d.querySelector('label[role="button"]');
-  const fileInput = d.querySelector('input[type="file"]');
-  const askQuestionWrapper = d.querySelector('#ask-question');
-  const postQuestionDirArea = d.querySelector('#post-questions-directive');
-  const askGroupBtn = d.querySelector('.ask-group-btn');
-  const commentBoxes = d.querySelectorAll('.comment-box .question-comment textarea');
+const mPhotosWrapper = document.querySelector('.meetup-photos__wrapper');
+const uploadBtn = document.querySelector('label[role="button"]');
+const fileInput = document.querySelector('input[type="file"]');
+const askGroupBtn = document.querySelector('.ask-group-btn');
+const commentBoxes = document.querySelectorAll('.comment-box .question-comment textarea');
+const imageUploadForm = document.getElementById('meetup-photos-upload-form');
+const uploadPhotosButton = document.getElementById('upload-photos__btn');
 
-  fileInput.onchange = (e) => {
-    const image = new Image();
-    const imageUrl = URL.createObjectURL(e.target.files[0]);
-    image.setAttribute('src', imageUrl);
+fileInput.onchange = (e) => {
+  const image = new Image();
+  const imageUrl = URL.createObjectURL(e.target.files[0]);
+  image.setAttribute('src', imageUrl);
+  uploadPhotosButton.classList.remove('hide');
+  uploadPhotosButton.classList.add('show');
+  mPhotosWrapper.appendChild(image);
+};
 
-    mPhotosWrapper.appendChild(image);
-  };
+// Tags
+const addTagBtn = document.querySelector('.btn__tag');
+const tagField = document.querySelector('input[id="tag"]');
+const addTagsContainer = document.querySelector('.meetup-tags-added');
 
-  // Tags
-  const addTagBtn = d.querySelector('.btn__tag');
-  const tagField = d.querySelector('input[id="tag"]');
-  const addTagsContainer = d.querySelector('.meetup-tags-added');
+let tags = null;
 
-  let tags = null;
+addTagBtn.onclick = (e) => {
+  // allowed separators for tags are commas and hashes (#)
+  tags = tagField.value.split(',');
+  if (tags.length === 1 && tags[0].includes('#')) {
+    tags = tags[0].split('#');
+  }
 
-  addTagBtn.onclick = (e) => {
-    // allowed separators for tags are commas and hashes (#)
-    tags = tagField.value.split(',');
-    if (tags.length === 1 && tags[0].includes('#')) {
-      tags = tags[0].split('#');
+  addTagsContainer.innerHTML = '';
+  tags.forEach((tag, i) => {
+    if (tag !== '') {
+      const span = d.createElement('span');
+
+      // to support deleting the tags later
+      span.id = i;
+      span.textContent = `#${tag}`;
+      addTagsContainer.appendChild(span);
     }
-
-    addTagsContainer.innerHTML = '';
-    tags.forEach((tag, i) => {
-      if (tag !== '') {
-        const span = d.createElement('span');
-
-        // to support deleting the tags later
-        span.id = i;
-        span.textContent = `#${tag}`;
-        addTagsContainer.appendChild(span);
-      }
-    });
-  };
-
-
-  commentBoxes.forEach((commentBox) => {
-    commentBox.oninput = function () {
-      const text = this.value;
-      // TODO: Implement auto-resize as user types feature
-    };
   });
+};
 
-  askGroupBtn.onclick = function () {
-    showEl({
-      elem: askQuestionWrapper,
-      activeClassName: 'active'
-    });
-    // User should not see
-    // post question directive
-    // after enabling ask questions UI
-    postQuestionDirArea.classList.add('inactive');
-    const divider = d.createElement('hr');
-    divider.classList.add('divider');
-    askQuestionWrapper.appendChild(divider);
-  };
+/* eslint-disable */
+const uploadMeetupImages = (images) => {
+  const meetupId = localStorage.getItem('activeMeetupId');
+  const userToken = localStorage.getItem('userToken');
+  const apiUrl = `http://localhost:9999/api/v1/meetups/${meetupId}/images`;
+  const formData = new FormData();
+  const data = {
+    meetupPhotos: images
+  }
+  for (const p in data) {
+    if (Object.prototype.hasOwnProperty.call(data, p)) {
+      formData.append(p, data[p]);
+    }
+  }
+  return fetch(apiUrl, {
+    headers: {
+      Authorization: `Bearer ${userToken}`
+    },
+    method: 'POST',
+    body: formData
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      throw new Error('Meetup images upload failed')
+    })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+}
 
-  /**
-   *
-   * @param {*} elem an object with Element and activeClassName props
-   * @returns {Element} elem
-   * @description Shows an hidden element, applying the styles in activeClassName to it
-   */
-  const showEl = ({ elem, activeClassName }) => {
-    elem.classList.add(activeClassName);
-    return elem;
-  };
-
-  // Meetup location on the map
-})();
+imageUploadForm.onsubmit = (e) => {
+  e.preventDefault();
+  const meetupPhotosInput = document.getElementById('meetupImage');
+  const imageFiles = meetupPhotosInput.files;
+  uploadMeetupImages(imageFiles)
+}
